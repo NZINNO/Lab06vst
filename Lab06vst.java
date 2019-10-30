708a95a4f8081df46c3ebc67a899b0668216dfdf
Zinno <4070806@vtsd.com>
2019-10-30 08:01:32 -0400

    Lab

--- Lab06vst.java
+++ Lab06vst.java
@@ -20,18 +20,51 @@ public class Lab06vst extends Applet
 
 		// Draw Random Lines
 
+				for (int k = 1; k <= 100; k++)
+				{
+					int x1 = (int)(Math.random() * 391) + 10;
+					int y1 = (int)(Math.random() * 291) + 10;
+					int x2 = (int)(Math.random() * 391) + 10;
+					int y2 = (int)(Math.random() * 291) + 10;
+					int red = (int)(Math.random() * 256);
+					int green = (int)(Math.random() * 256);
+					int blue = (int)(Math.random() * 256);
+					g.setColor(new Color(red,green,blue));
+					g.drawLine(x1,y1,x2,y2);
+				}
 
 
 
 
 		// Draw Random Squares
 
-
+		for (int k = 1; k <= 100; k++)
+		{
+			int x1 = (int)(Math.random() * 341 ) + 400;
+			int y1 = (int)(Math.random() * 241) + 10;
+			int x2 = (int)(Math.random() * 341) + 400;
+			int y2 = (int)(Math.random() * 241) + 10;
+			int red = (int)(Math.random() * 256);
+			int green = (int)(Math.random() * 256);
+			int blue = (int)(Math.random() * 256);
+			g.setColor(new Color(red,green,blue));
+			g.fillRect (x1,y1,45,45);
+		}
 
 
 
 		// Draw Random Circles
-
+		for (int k = 1; k <= 100; k++)
+		{
+			int x1 = (int)(Math.random() * 391) + 10;
+			int y1 = (int)(Math.random() * 291) + 10;
+			int x2 = (int)(Math.random() * 391) + 10;
+			int y2 = (int)(Math.random() * 291) + 10;
+			int red = (int)(Math.random() * 256);
+			int green = (int)(Math.random() * 256);
+			int blue = (int)(Math.random() * 256);
+			g.setColor(new Color(red,green,blue));
+			g.drawOval (x1,y1,x2,y2);
 
 
 
@@ -45,8 +78,3 @@ public class Lab06vst extends Applet
 	}
 
 }
-
-
-
-
- 
