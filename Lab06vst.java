5cc684c70e5fa395e78abe056237e951b1685ddc
Zinno <4070806@vtsd.com>
2019-10-30 08:43:23 -0400

    lab

--- Lab06vst.java
+++ Lab06vst.java
@@ -56,7 +56,7 @@ public class Lab06vst extends Applet
 		// Draw Random Circles
 		for (int k = 1; k <= 100; k++)
 		{
-			int x1 = (int)(Math.random() * 391) + 10;
+			int x1 = (int)(Math.random() * 391) + 400;
 			int y1 = (int)(Math.random() * 291) + 10;
 			int x2 = (int)(Math.random() * 391) + 10;
 			int y2 = (int)(Math.random() * 291) + 10;
@@ -65,7 +65,7 @@ public class Lab06vst extends Applet
 			int blue = (int)(Math.random() * 256);
 			g.setColor(new Color(red,green,blue));
 			g.drawOval (x1,y1,x2,y2);
-
+		}
 
 
 
